07a2791c6f0b26c0ac95391c4cdb77051ce84cf8
aryandev1 <55704643+aryandev1@users.noreply.github.com>
2019-09-23 22:48:14 +0530

    ToDo List App Using Angular

---
+++ src/app/todo/todofooter/todofooter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todofooter',
+  templateUrl: './todofooter.component.html',
+  styleUrls: ['./todofooter.component.css']
+})
+export class TodofooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/todo/todoinput/todoinput.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todoinput',
+  templateUrl: './todoinput.component.html',
+  styleUrls: ['./todoinput.component.css']
+})
+export class TodoinputComponent implements OnInit {
+
+  constructor() { }
+  str=""
+  ngOnInit() {
+  }
+}
